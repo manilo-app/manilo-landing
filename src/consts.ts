5d680c301ad20c0eba9d2bff5e54e0ffb6228057
Borys Harholinskyi <gargolinsky@outlook.com>
2026-08-23 11:09:07 +0200

    go to astro

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const SITE = 'https://manilo.app';
+export const SITE_NAME = 'Manilo';
+export const APP_STORE_URL = 'https://apps.apple.com/app/id6761738170';
+export const APP_STORE_ID = '6761738170';
+export const DASHBOARD_URL = 'https://dashboard.manilo.app';
+export const SUPPORT_EMAIL = 'support@manilo.app';
